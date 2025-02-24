f6eddd3fe9b1b068fd748012c2c65f3695658ba9
rogeriojr <rogeriojr1100@gmail.com>
2025-02-23 23:57:30 -0300

    config: vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,40 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
-import { resolve } from 'path'
-import sass from 'sass'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
+import { resolve } from "path";
+import sass from "sass";
 
-export default defineConfig({
-  plugins: [
-    vue({
-      template: { transformAssetUrls }
-    }),
-    quasar({
-      sassVariables: resolve(__dirname, 'src/styles/quasar-variables.sass')
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  // Carrega as variáveis do .env
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [
+      vue({
+        template: { transformAssetUrls },
+      }),
+      quasar({
+        sassVariables: resolve(__dirname, "src/styles/quasar-variables.sass"),
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-    port: 3000,
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        quietDeps: true,
-        implementation: sass
-      }
-    }
-  }
-})
+    define: {
+      "import.meta.env.VITE_API_URL": JSON.stringify(env.VITE_API_URL),
+    },
+    server: {
+      port: 3000,
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          quietDeps: true,
+          implementation: sass,
+        },
+      },
+    },
+  };
+});
