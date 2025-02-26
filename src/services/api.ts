618a6c8f2c3cf6d374a90d41941f5f1c2ac5d53a
rogeriojr <rogeriojr1100@gmail.com>
2025-02-26 13:02:38 -0300

    fix: corrections and update

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useAuthStore } from '@/stores/auth'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://demometaway.vps-kinghost.net:8485/api',
   headers: {
     'Content-Type': 'application/json'
   }
